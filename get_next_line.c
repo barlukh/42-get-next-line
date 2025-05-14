99e5320f702ada9730b0474a755e4bc6fe600b0a
Boris Gazur <barlukh@gmail.com>
2025-05-14 11:47:30 +0300

    First prototype

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,32 @@
 /*   By: bgazur <bgazur@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/12 09:17:16 by bgazur            #+#    #+#             */
-/*   Updated: 2025/05/12 09:17:18 by bgazur           ###   ########.fr       */
+/*   Updated: 2025/05/14 11:43:18 by bgazur           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+char	*get_next_line(int fd)
+{
+	char		buffer[BUFFER_SIZE];
+	static char	*cache;
+	ssize_t		bytes_read;
+	ssize_t		i;
+	char		*ret;
+
+	bytes_read = read(fd, buffer, BUFFER_SIZE);
+	if (bytes_read == 0 || bytes_read == -1)
+		return (NULL);
+
+	cache = ft_strdup(buffer);
+
+	i = 0;
+	while (*cache != '\n' && i < bytes_read)
+	{
+		cache++;
+		i++;
+	}
+	ret = ft_substr(cache - i, 0, i + 1);
+	return (ret);
+}
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,52 @@
 /*   By: bgazur <bgazur@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/12 09:17:53 by bgazur            #+#    #+#             */
-/*   Updated: 2025/05/12 09:17:55 by bgazur           ###   ########.fr       */
+/*   Updated: 2025/05/14 10:44:59 by bgazur           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1024
+# endif
+
+/** Copies bytes from one memory area to another; the areas must not overlap
+ * @param dest Pointer to the destination memory area
+ * @param src Pointer to the source memory area
+ * @param n Number of bytes to copy
+ * @return Pointer to the destination memory area
+ */
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+
+/** Duplicates a string using dynamic memory allocation
+ * @param s String to duplicate
+ * @return Pointer to the duplicated string, 'NULL' if the allocation fails
+ */
+char	*ft_strdup(const char *s);
+
+/** Calculates the length of a string
+ * @param s String to calculate the length of
+ * @return Number of bytes in the passed string
+ */
+size_t	ft_strlen(const char *s);
+
+/** Creates a substring using dynamic memory allocation
+ * @param s String to make the substring from
+ * @param start Starting index
+ * @param len Length of the substring
+ * @return Pointer to the new substring, 'NULL' if the allocation fails
+ */
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+
+/** Reads and returns a line from a file pointed to by a file descriptor
+ * @param fd File descriptor to read from
+ * @return String (line) that was read
+ */
+char	*get_next_line(int fd);
+
+#endif
